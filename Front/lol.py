d89522935cf9dd9a3b816fcdbccbecf93aa5347b
Arion67 <arion.grez@inacapmail.cl>
2026-04-13 00:08:13 -0400

    13-04-2026

---
+++ Front/lol.py
@@ -0,0 +1,19 @@
+
+{
+  "_id": { "$oid": "65f2a1b2c3d4e5f6a7b8c9d0" },
+  "nro_pedido": "V-2026-001",
+  "cliente": {
+    "nombre": "Zen",
+    "email": "zen@ingenieria.cl"
+  },
+  "items": [
+    { "producto": "Zapatilla Runner Pro", "cant": 1, "precio": 59990 }
+  ],
+  "total": 59990,
+  "estado": "completado"
+}
+
+
+
+
+
